05928c2c06bfeebaddf10dcc0010abbb5cc150aa
bws9000 <snyder.burt@gmail.com>
2025-08-01 18:38:12 -0400

    wip: update module-info and Gradle config

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.burtsnyder.boxrift.app {
+    requires javafx.controls;
+    requires javafx.graphics;
+    exports com.burtsnyder.boxrift.ui.javafx.view;
+    exports com.burtsnyder.boxrift.core.engine;
+}
